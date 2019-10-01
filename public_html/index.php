7551783f44e11e045aa3b1df805385477cea8bc5
DESKTOP-RTHLK8R\georg <Georgesquair@gmail.com>
2019-10-01 11:43:50 +0100

    index.php added with placeholder form, initial stylesheet, some folder structures forming

---
+++ public_html/index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+
+<html>
+
+<head>
+	<link rel="stylesheet" href="../css/stylesheet.css">
+	<link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<h1>Currency Conversion API</h1>
+	<div id="form-container">
+		<form>
+			<input type="text" name="field1" placeholder="field1">
+			<input type="text" name="field2" placeholder="field2">
+			<input type="text" name="field3" placeholder="field3">
+			<input type="submit" name="submitRequest" value="Submit">
+
+			<textarea rows="4" cols="50" placeholder="Report things here..."></textarea>
+		</form>
+	</div>
+</body>
+
+</html>
