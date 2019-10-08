c2aa6b4cfd3655b51bffd4f826e2c225cb78b9a7
DESKTOP-RTHLK8R\georg <Georgesquair@gmail.com>
2019-10-08 15:10:14 +0100

    Removes line originally used to test adding currency

--- resources/api/get.php
+++ resources/api/get.php
@@ -5,7 +5,6 @@ require_once ("../libary/currencyFunctions.php");
 XMLOperation::invoke(function($f){
 	$filePath = "../xml/test.xml";
 	$xpathQuery = $_POST['xpath'];
-	$newCurrency = createNewCurrency("GBP", "England", "£", 1.0);
 		return $f
 			->setFilePath($filePath)
 			->getElements($xpathQuery);
